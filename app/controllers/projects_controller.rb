7804bc4243b5565fe2e30f5ec825ac518affc97f
Steve Dodkin <stevedodkin@mac.com>
2013-03-26 16:11:34 +0000

    Implement creating tickets for a project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -40,8 +40,7 @@ class ProjectsController < ApplicationController
 		def find_project
 			@project = Project.find(params[:id])
 			rescue ActiveRecord::RecordNotFound
-				flash[:alert] = "The project you were looking" + 
-												" for could not be found."
+				flash[:alert] = "The project you were looking for could not be found."
 				redirect_to projects_path
 		end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Project < ActiveRecord::Base
+	has_many :tickets
 	validates :name, presence: true
 end
